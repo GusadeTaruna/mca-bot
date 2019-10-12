14ba5e12baf8899b53eab0314b4fe86fba5087ad
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-12 13:53:59 -0700

    hmm

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ if($method == 'POST'){
 	$kata = $json->queryResult->parameters->kata;
 
 	if (in_array($kata, $welcome)) {
-    	$balasan = "Selamat datang di Naybot!". PHP_EOL .
-    				"Ada yang bisa aku bantu ?". PHP_EOL .
-    				"(Jalankan perintah listperintah untuk melihat perintah yang tersedia)" ;
+    	$balasan = "Selamat datang di Naybot!
+    				Ada yang bisa aku bantu ?
+    				(Jalankan perintah listperintah untuk melihat perintah yang tersedia)" ;
 	}else{
 		$balasan = "input tidak terdaftar";
 	}
