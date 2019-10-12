b0efd92489b889e5c34257abf274137bdb4d04f2
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-11 22:01:30 -0700

    webhook php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+	
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($method="POST"){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, salam kenal!";
+			break;
+		case 'bye':
+			$speech = "Terima kasih !";
+			break;
+		case 'anything':
+			$speech = "coba ketik semua";
+			break;
+		
+		default:
+			$speech = "Maaf inputan tidak dikenali";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech= "";
+	$response->displayText= "";
+	$response->source= "";
+	echo json_encode($response);
+
+
+
+}
+else
+{
+	echo "Method tidak diijinkan";
+}
+
+
+?>
