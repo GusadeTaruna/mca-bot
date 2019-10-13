e0e00269b3ebcd50223ae8cfe4a0a5a66650d8f5
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-12 23:34:52 -0700

    read

--- index.php
+++ index.php
@@ -21,6 +21,21 @@ if($method == 'POST'){
 		$responPerintah = "LIST PERINTAH YANG TERSEDIA\n1. booking (Untuk pesan resource)\n2. lihatresource (Untuk melihat ketersediaan resource)\n3. lihatdatapinjam (Untuk melihat data peminjaman resource)";
 	}
 
+	//perintah 1
+	if(in_array($kata, $perintah1)){
+		$responPerintah1 = "Untuk booking resource, anda perlu menginput Kode Karyawan terlebih dahulu";
+		$sql = 'SELECT * FROM tb_karyawan where kode_karyawan = "$kata"';
+		$hasil = mysqli_query($conn, $sql);
+		if (mysqli_num_rows($result) > 0) {
+		    // output data of each row
+		    while($row = mysqli_fetch_assoc($result)) {
+		    	$responPerintah1 = "Halo" . $row["nama_karyawan"]. " anda mau booking apa ?";
+		    }
+		} else {
+		    $responPerintah1 = "Data Karyawan tidak ditemukan";
+		}
+	}
+
 	// switch ($kata) {
 	// 	case 'hi':
 	// 		$speech = "awaw";
@@ -47,10 +62,15 @@ if($method == 'POST'){
 	if (in_array($kata, $welcome)){
 		$response->fulfillmentText = $balasan;
 	
-	}elseif (in_array($kata, $perintah)) {
+	}
+	elseif (in_array($kata, $perintah)) {
 		$response->fulfillmentText = $responPerintah;
 	
-	}else{
+	}
+	elseif (in_array($kata, $perinta1)) {
+		$response->fulfillmentText = $responPerintah1;
+	}
+	else{
 		$response->fulfillmentText = "Inputanmu tidak dapat dikenali, Silahkan jalankan perintah listperintah untuk melihat perintah yang tersedia";
 	}
 	$response->source = "webhook";
