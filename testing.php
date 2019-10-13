ab07b33de15d3c87c753974806cfd4adec24bbc1
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-13 16:17:57 -0700

    test

--- testing.php
+++ testing.php
@@ -1,18 +1,41 @@
 <?php
     require 'koneksi.php';
     function getWardInfo($param){
+        // $wardinfo="";
+        // $Query="SELECT * FROM tb_karyawan WHERE kode_karyawan=$param";
+        // $Result=pg_query($conn,$Query);
+        // if(isset($Result) && !empty($Result) && pg_num_rows($Result) > 0){
+        //     $row=pg_fetch_assoc($Result);
+        //     $wardinfo= "Halo" . $row["nama_karyawan"];
+        //     $arr=array(
+        //         "fulfillmentText" => $wardinfo,
+        //     );
+        // }else{
+        //     $arr=array(
+        //         "fulfillmentText" => "Gak nemu",
+        //     );
+        //     sendMessage($arr);
+        // }
+
         $wardinfo="";
         $sql = 'SELECT * FROM tb_karyawan where kode_karyawan = "$kata"';
         $hasil = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) > 0) {
          // output data of each row
             while($row = mysqli_fetch_assoc($result)) {
-                 $wardinfo = "Halo" . $row["nama_karyawan"]. " anda mau booking apa ?";
+                $wardinfo = "Halo" . $row["nama_karyawan"]. " anda mau booking apa ?";
+                $arr=array(
+                    "fulfillmentText" => $wardinfo,
+                );      
             }
-        } else {
-                $wardinfo = "Data Karyawan tidak ditemukan";
+        } 
+        else {
+            $arr=array(
+                "fulfillmentText" => "Data tidak ditemukan",
+            );   
         }
         $arr = $wardinfo;
         sendMessage($arr);
+
     }
 ?>
