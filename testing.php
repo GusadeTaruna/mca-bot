b9fbd41371c18fcca3ed03728e388fb7f7d1716b
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-13 12:01:25 -0700

    gmm

--- testing.php
+++ testing.php
@@ -1,17 +1,34 @@
 <?php
     require 'koneksi.php';
     function getWardInfo($param){
+        // $wardinfo="";
+        // $Query="SELECT * FROM tb_karyawan WHERE kode_karyawan=$param";
+        // $Result=mysqli_query($conn,$Query);
+        // if(isset($Result) && !empty($Result) && mysqli_num_rows($Result) > 0){
+        //     $row=mysqli_fetch_assoc($Result);
+        //     $wardinfo= "Halo" . $row["nama_karyawan"];
+        //     $arr=array(
+        //         "fulfillmentText" => $wardinfo,
+        //     );
+        //     sendMessage($arr);
+        // }else{
+        //     $arr=array(
+        //         "fulfillmentText" => "Gak nemu",
+        //     );
+        //     sendMessage($arr);
+        // }
         $wardinfo="";
-        $Query="SELECT * FROM tb_karyawan WHERE kode_karyawan=$param";
-        $Result=mysqli_query($conn,$Query);
-        if(isset($Result) && !empty($Result) && mysqli_num_rows($Result) > 0){
-            $row=mysqli_fetch_assoc($Result);
-            $wardinfo= "Halo" . $row["nama_karyawan"];
-            $arr=array(
-                "fulfillmentText" => $wardinfo,
-            );
+        $sql = "SELECT * FROM tb_karyawan WHERE kode_karyawan = $param";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                $wardinfo= "Halo" . $row["nama_karyawan"];
+                $arr=array(
+                    "fulfillmentText" => $wardinfo,
+                );
+            }
             sendMessage($arr);
-        }else{
+        } else {
             $arr=array(
                 "fulfillmentText" => "Gak nemu",
             );
