025eb24e98bd5be14844ae1a0fc959fa14a8c4ec
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-13 11:53:47 -0700

    TEST

--- testing.php
+++ testing.php
@@ -4,10 +4,9 @@
         $wardinfo="";
         $Query="SELECT * FROM tb_karyawan WHERE kode_karyawan=$param";
         $Result=mysqli_query($conn,$Query);
-        if (mysqli_num_rows($result) > 0) {
-            while($row = mysqli_fetch_assoc($result)) {
-                $wardinfo= "Halo" . $row["nama_karyawan"];
-            }
+        if(isset($Result) && !empty($Result) && mysqli_num_rows($Result) > 0){
+            $row=mysqli_fetch_assoc($Result);
+            $wardinfo= "Halo" . $row["nama_karyawan"];
             $arr=array(
                 "fulfillmentText" => $wardinfo,
             );
