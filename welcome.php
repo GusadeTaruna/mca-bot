90e26edae11826fe909ceb4a956d4c5e02484a69
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-12 23:35:07 -0700

    reade data

--- welcome.php
+++ welcome.php
@@ -1,5 +1,8 @@
 <?php
 	$welcome = array("halo","hai","hey","hy","helo","hello","hallo","hei","we","yo","selamat siang",
 		"selamat malam","selamat pagi");
+
 	$perintah = array("listperintah","list","perintah","list perintah");
+	
+	$perintah1 = array("booking","booking resource","saya mau booking","pesan resource");
 ?>
