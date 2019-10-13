6ecd3c1994702b1fdefccf57dfa9cbdbd2499bbc
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-13 01:43:36 -0700

    F

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include 'welcome.php';
 include 'koneksi.php';
+session_start();
 $method = $_SERVER['REQUEST_METHOD'];
 // $state = 0;
 
@@ -36,22 +37,23 @@ if($method == 'POST'){
 	}
 
 	//perintah 1
-	if($kata=="booking"){
-		$responPerintah1 = "Untuk booking resource, anda perlu menginput Kode Karyawan terlebih dahulu";
-	}
-	if(!$kata){
-		$responPerintah1= "Anda belum memasukan ID Karyawan";
-	}else{
-		$sql = 'SELECT * FROM tb_karyawan where kode_karyawan = "$kata"';
-		$hasil = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($hasil) > 0) {
-		    // output data of each row
-		    while($row = mysqli_fetch_assoc($hasil)) {
-		    	$responPerintah1 = "Halo" . $row["nama_karyawan"]. " anda mau booking apa ?";
-		    }
-		} else {
-		    $responPerintah1 = "Data Karyawan tidak ditemukan";
-		}
+	if($kata="booking")){
+			if($_SESSION['booking']){
+				$sql = 'SELECT * FROM tb_karyawan where kode_karyawan = "$kata"';
+				$hasil = mysqli_query($conn, $sql);
+				if (mysqli_num_rows($hasil) > 0) {
+				    // output data of each row
+				    while($row = mysqli_fetch_assoc($hasil)) {
+				    	$responPerintah1 = "Halo" . $row["nama_karyawan"]. " anda mau booking apa ?";
+				    }
+				} else {
+				    $responPerintah1 = "Data Karyawan tidak ditemukan";
+				}
+			}
+			else{
+				$_SESSION['booking'] = TRUE;
+				$responPerintah1 = "Untuk booking resource, anda perlu menginput Kode Karyawan terlebih dahulu";
+			}
 	}
 	
 
