d97ca581047371a19a8220f8da4918f608d7d0db
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-12 23:07:00 -0700

    db sukses

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ if($method == 'POST'){
 	$param = $json->queryResult->parameters->kata;
 	$kata = strtolower($param);
 	//Respon untuk percakapan awal
-	// if (in_array($kata, $welcome)) {
- //    	$balasan = "Selamat datang di Naybot!, Ada yang bisa aku bantu ?\n(Jalankan perintah listperintah untuk melihat perintah yang tersedia)" ;
-	// }
+	if (in_array($kata, $welcome)) {
+    	$balasan = "Selamat datang di Naybot!, Ada yang bisa aku bantu ?\n(Jalankan perintah listperintah untuk melihat perintah yang tersedia)" ;
+	}
 
-	// //Respon untuk lihat perintah
-	// if(in_array($kata, $perintah)){
-	// 	$responPerintah = "LIST PERINTAH YANG TERSEDIA\n1. booking (Untuk pesan resource)\n2. lihatresource (Untuk melihat ketersediaan resource)\n3. lihatdatapinjam (Untuk melihat data peminjaman resource)";
-	// }
+	//Respon untuk lihat perintah
+	if(in_array($kata, $perintah)){
+		$responPerintah = "LIST PERINTAH YANG TERSEDIA\n1. booking (Untuk pesan resource)\n2. lihatresource (Untuk melihat ketersediaan resource)\n3. lihatdatapinjam (Untuk melihat data peminjaman resource)";
+	}
 
 	// switch ($kata) {
 	// 	case 'hi':
@@ -40,19 +40,19 @@ if($method == 'POST'){
 	// }
 
 	$response = new \stdClass();
-	if (!$conn) {
-    	$response->fulfillmentText = "sukses";
-	}
-	$response->fulfillmentText = "failed";
-	// if (in_array($kata, $welcome)){
-	// 	$response->fulfillmentText = $balasan;
-	
-	// }elseif (in_array($kata, $perintah)) {
-	// 	$response->fulfillmentText = $responPerintah;
-	
-	// }else{
-	// 	$response->fulfillmentText = "Inputanmu tidak dapat dikenali, Silahkan jalankan perintah listperintah untuk melihat perintah yang tersedia";
+	// if (!$conn) {
+ 	//    $response->fulfillmentText = "failed";
 	// }
+	// $response->fulfillmentText = "sukses";
+	if (in_array($kata, $welcome)){
+		$response->fulfillmentText = $balasan;
+	
+	}elseif (in_array($kata, $perintah)) {
+		$response->fulfillmentText = $responPerintah;
+	
+	}else{
+		$response->fulfillmentText = "Inputanmu tidak dapat dikenali, Silahkan jalankan perintah listperintah untuk melihat perintah yang tersedia";
+	}
 	$response->source = "webhook";
 	echo json_encode($response);
 }
